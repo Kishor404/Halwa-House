5e8013ea5b975625f74e9dcb77899f5e759d794b
Kishor404 <149653325+Kishor404@users.noreply.github.com>
2024-11-02 14:56:51 +0530

    Navigation Set

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,18 @@
 import "./components.css";
+import { Link } from 'react-router-dom';
 
 const Navbar=()=>{
     return(
         <>
         <section>
             <p>Navbar</p>
+            <div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/Sweets"}>Sweets</Link>
+                <Link to={"/Halwa"}>Halwa</Link>
+                <Link to={"/About"}>About</Link>
+                <Link to={"/Contact"}>Contact</Link>
+            </div>
         </section>
         </>
     );
