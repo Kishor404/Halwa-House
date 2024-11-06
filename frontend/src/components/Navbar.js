f891c61f45a9d646718729838ddf2f5a8a89bae1
Harikrishna390 <harikrishnarmh28@gmail.com>
2024-11-06 20:09:56 +0530

    navbar -harikrishna

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,38 @@
 import "./components.css";
-import { Link } from 'react-router-dom';
+import "../Styles/Navbar.css";
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navdiff">
+                <div className="navbar-logo">
+                    <img src="icons\1.png" alt="Lala Halwa House Logo" className="logo" />
+                    <h1>Lala Halwa House</h1>
+                    <div className="navbar-search">
+                        <input type="text" placeholder="Let's Eat" />
+                        <button type="submit" className="search-button">
+                            
+                        </button>
+                        <img src="icons/search.svg" alt="" width="15" height="25" />
+                    </div>
+                    <div className="navbar-account">
+                        <img src="icons/account.svg" alt="" width="25" height="26" />
+                    </div>
+                </div>
 
-const Navbar=()=>{
-    return(
-        <>
-        <section>
-            <p>Navbar</p>
-            <div>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/Sweets"}>Sweets</Link>
-                <Link to={"/Halwa"}>Halwa</Link>
-                <Link to={"/About"}>About</Link>
-                <Link to={"/Contact"}>Contact</Link>
+
+                <div className="nav-menu">
+                    <ul className="navbar-links">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#sweets">Sweets</a></li>
+                        <li><a href="#halwa">Halwa</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+
+                </div>
+
             </div>
-        </section>
-        </>
+        </nav>
     );
-};
-
+}
 export default Navbar;
