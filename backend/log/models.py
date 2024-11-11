d66cf7bbb9f6ea05e552e410f579b7b9bdf61e6a
Kishor404 <149653325+Kishor404@users.noreply.github.com>
2024-11-11 19:24:13 +0530

    Backend - Password Encryption

--- backend/log/models.py
+++ backend/log/models.py
@@ -1,16 +1,23 @@
 # log/models.py
 
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class Log(models.Model):
-    name = models.CharField(max_length=255,blank=False,null=False)
-    password=models.CharField(max_length=255,blank=False,null=False)
-    address=models.CharField(max_length=255,blank=True,null=True)
-    email=models.CharField(max_length=255,blank=True,null=True,unique=True)
-    phone=models.CharField(max_length=255,blank=False,null=False,unique=True)
-    dob=models.CharField(max_length=255,blank=False,null=False)
-    cart=models.JSONField(default={})
+    name = models.CharField(max_length=255, blank=False, null=False)
+    password = models.CharField(max_length=255, blank=False, null=False)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True, unique=True)
+    phone = models.CharField(max_length=255, blank=False, null=False, unique=True)
+    dob = models.CharField(max_length=255, blank=False, null=False)
+    cart = models.JSONField(default={})
     created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Hash the password before saving
+        if not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
--- backend/log/serializers.py
+++ backend/log/serializers.py
@@ -6,4 +6,5 @@ from .models import Log
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
-        fields = ['id','name','password','address','email','phone','created']
+        fields = ['id', 'name', 'password', 'address', 'email', 'phone', 'created']
+        extra_kwargs = {'password': {'write_only': True}}  # Makes password write-only
